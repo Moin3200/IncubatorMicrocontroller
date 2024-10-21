b7ac3766398f9fecd15749329402e7a4a883f9e4
Moin3200 <moeen.hassan@devflovv.com>
2024-10-21 19:00:06 +0500

    Keypad code
    this is keypad code to adjust the keypad in my code.

---
+++ sketch_oct18a.ino
@@ -0,0 +1,27 @@
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+   {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = {A5, A4, 10, 3};
+byte colPins[COLS] = {A0, A1, A2, A3};
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+}
